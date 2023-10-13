d36fa80afc8f97e2e21ae7b1c3cc90e86b4d7a29
markusVJH <markus.heinila@pp.inet.fi>
2023-10-13 12:03:01 +0300

    testing if products contain all  data, and if product 1 has the specific detailed data

--- src/__tests__/sliceTests/productSlice.test.js
+++ src/__tests__/sliceTests/productSlice.test.js
@@ -29,3 +29,33 @@ describe("fetchProducts", () => {
   })
 
 })
+
+it("Products should contain data that is equal to our test case", async () => {
+  const store = mockStore({ products: [] });
+  await store.dispatch(fetchProducts());
+
+  const actions = store.getActions();
+  const products = actions[1].payload;
+
+  products.forEach((product) => {
+    expect(product).toHaveProperty("id");
+    expect(product).toHaveProperty("title");
+    expect(product).toHaveProperty("description");
+    expect(product).toHaveProperty("price");
+    expect(product).toHaveProperty("category");
+    expect(product).toHaveProperty("image");
+  })
+})
+it("Product 1 has specific data to our test case", async () => {
+  const store = mockStore({ products: [] });
+  await store.dispatch(fetchProducts());
+
+  const actions = store.getActions();
+  const products = actions[1].payload;
+  expect(products[0].id).toEqual(1)
+  expect(products[0].title).toEqual("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+  expect(products[0].description).toEqual("Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday")
+  expect(products[0].price).toEqual(109.95)
+  expect(products[0].image).toEqual("https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg")
+  expect(products[0].category).toEqual("men's clothing")
+  })
