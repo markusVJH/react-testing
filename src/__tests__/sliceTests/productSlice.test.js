4db194b234bcdca12d25519705f5a62ff0822cc0
markusVJH <markus.heinila@pp.inet.fi>
2023-10-13 11:25:52 +0300

    fetchproducts test, imports fixed

--- src/__tests__/sliceTests/productSlice.test.js
+++ src/__tests__/sliceTests/productSlice.test.js
@@ -1,4 +1,8 @@
-const { default: createMockStore } = require("redux-mock-store")
+import thunk from "redux-thunk"
+import configureStore from "redux-mock-store"
+import { fetchProducts } from "../../features/productsSlice"
+
+const mockStore = configureStore([thunk])
 
 describe('a test', () => {
   it('should pass', () => {
@@ -11,8 +15,17 @@ describe("fetchProducts", () => {
   beforeEach(() => {
     jest.setTimeout(10000); // 10 second timeout
   })
-})
 
-it("should fetch products from the API", async () => {
-  const store = createMockStore({ products: [] });
+  it("should fetch products from the API", async () => {
+    const store = mockStore({ products: [] });
+    await store.dispatch(fetchProducts());
+
+    const actions = store.getActions();
+    expect(actions[0].type).toEqual("products/fetchProducts/pending");
+    expect(actions[1].type).toEqual("products/fetchProducts/fulfilled");
+
+    const products = actions[1].payload;
+    expect(products).toHaveLength(20);
+  })
+
 })
