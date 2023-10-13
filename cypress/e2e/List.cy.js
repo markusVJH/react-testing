a5d7e576b5756b5fe9de6913aad27945b355fa77
markusVJH <markus.heinila@pp.inet.fi>
2023-10-13 14:55:51 +0300

    cypress installation and small tests

---
+++ cypress/e2e/List.cy.js
@@ -0,0 +1,16 @@
+describe('List component test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  it("Page and Title render", () => {
+    cy.get('h1').should('contain', 'List will be here')
+  })
+  it("Page products load", () => {
+    cy.get('[data-testid="product"]', {timeout: 4000}).should('have.length', 20)
+  })
+  it("List link to Cart works", () => {
+    cy.get('[data-testid="cart-link"]').click()
+    cy.url().should('include', '/cart')
+    cy.get('h1').should('contain', 'Cart')
+  })
+})
